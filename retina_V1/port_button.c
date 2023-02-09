2cb0c04db588e6b21cf9e05888d273168db3c5a1
josueportiz <josueportiz@gmail.com>
2023-02-09 20:43:27 +0100

    Doxygen button ISR

--- retina_V1/port_button.c
+++ retina_V1/port_button.c
@@ -35,7 +35,20 @@ void port_button_init(uint32_t button_id)
 // INTERRUPT SERVICE ROUTINES
 //------------------------------------------------------
 /**
- * @brief This function handles Px10-Px15 global interrupts.
+ * @brief  This function handles Px10-Px15 global interrupts.
+ * 
+ * First, this function identifies the line/ pin which has raised the interruption. Then, perform the desired action. Before leaving it cleans the interrupt pending register.
+ *
+ * > **TO-DO alumnos**
+ * > 
+ * > ✅ 1. Read the value of the GPIO og the user button. Use the #BUTTON_0_ID index to get the port and pin from the `buttons_arr` \n
+ * > ✅ 2. If the value is #HIGH means that the button as been released, therefore set the field `flag_pressed` to `false` \n
+ * > ✅ 3. If the value is #LOW means that the button as been pressed, therefore set the field `flag_pressed` to `true` \n
+ * > ✅ 4. Clean the corresponding bit of the `PR` register \n
+ * > &nbsp;&nbsp;&nbsp;&nbsp;💡 Recommendation: use the macro #BIT_POS_TO_MASK \n 
+ * > &nbsp;&nbsp;&nbsp;&nbsp;💡 **To clean an interrupt in the *Pending Register* (`PR`) we must write '1' in the corresponding bit**. *Look at the example in section "3.9.7. Ejemplo de interrupción externa" of the reference book.* You can also find this out by consulting the *reference manual* (section "10.3.6 Pending register (EXTI_PR)") * \n 
+ * \n
+ * 
  */
 void EXTI15_10_IRQHandler(void)
 {
